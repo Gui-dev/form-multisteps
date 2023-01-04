29dc867f87f3ac1b346480edc9d0c2742b12b7a5
Gui-dev <adrianon10@gmail.com>
2023-01-04 04:30:30 -0300

    style: add App style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,68 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { FiSend } from 'react-icons/fi'
+
+import { UserForm } from './components/UserForm'
+import { ReviewForm } from './components/ReviewForm'
+import { Thanks } from './components/Thanks'
+
+import { useForm } from './hooks/useForm'
+
+import './index.css'
+
 function App () {
+  const formComponents = [
+    <UserForm />,
+    <ReviewForm />,
+    <Thanks />,
+  ]
+  const { changeStep, currentComponent, currentStep, isFirstStep, isLastStep } = useForm(formComponents)
+
   return (
-    <h1>Hello World</h1>
+    <section className="app">
+      <header className="header">
+        <h1>Deixe sua avaliação</h1>
+        <p>
+          Ficamos felizes com sua compra,
+          utilize o formulário abaixo para avaliar o produto
+        </p>
+      </header>
+      <div className="form-container">
+        <p>Etapas</p>
+
+        <form onSubmit={event => changeStep({ index: currentStep + 1, event })}>
+          <div className="inputs-container">
+            {currentComponent}
+          </div>
+          <div className="actions">
+            <button
+              type="button"
+              onClick={() => changeStep({ index: currentStep - 1 })}
+              disabled={isFirstStep}
+            >
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            {
+              !isLastStep && (
+                <button type="submit">
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              )
+            }
+            {
+              isLastStep && (
+                <button type="submit">
+                  <span>Enviar</span>
+                  <FiSend />
+                </button>
+              )
+            }
+
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
