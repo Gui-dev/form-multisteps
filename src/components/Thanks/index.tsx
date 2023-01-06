09c887d512ff34d99cfb9f4b314b450e3a382b86
Gui-dev <adrianon10@gmail.com>
2023-01-06 01:24:54 -0300

    feat: add component Thanks

---
+++ src/components/Thanks/index.tsx
@@ -0,0 +1,37 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill
+} from 'react-icons/bs'
+
+import { DataProps } from "../../App"
+
+import './style.css'
+
+type ThanksProps = {
+  data: DataProps
+}
+
+export const Thanks = ({ data }: ThanksProps) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você receberá um cupom de 10% de desconto
+        para a sua próxima compra
+      </p>
+      <p>
+        Para concluir sua avaliação clique no botão de Enviar abaixo
+      </p>
+      <h3>Aqui está o resumo da sua avaliação</h3>
+      <p className="review-data">
+        <span>Satisfação com o produto: {data.review}</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+        {data.comment}
+      </p>
+    </div>
+  )
+}
